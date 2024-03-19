8561e1cc5fa07ff065f71f966f72143d5d79ac28
Marcos Pereira <marcos.p.10@hotmail.com>
2024-03-19 17:38:20 -0300

    feature: change language

--- src/components/ComingSoonComponent/index.tsx
+++ src/components/ComingSoonComponent/index.tsx
@@ -1,11 +1,64 @@
-import Link from 'next/link';
+"use client"
 
 import styles from './styles.module.scss';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 
 export const ComingSoonComponent = (): React.JSX.Element => {
     const imageDim = [880, 540]
-    const multiple = 1.9
+    const [multiple, setMultiple] = useState<number>(1.9);
+    const [screenWidth, setScreenWidth] = useState<number | null>(null)
+    const [currentWelcome, setCurrentWelcome] = useState<string>("coming soon")
+
+    const welcomeArray: string[] = [
+        "coming soon",
+        "próximamente",
+        "bientôt disponible",
+        "bald verfügbar",
+        "presto disponibile",
+        "em breve",
+        "binnenkort beschikbaar",
+        "скоро",
+        "即将推出",
+        "即將推出",
+        "近日公開",
+        "곧 출시될 예정입니다",
+        "قريبًا",
+        "çok yakında",
+        "जल्द ही आ रहा है",
+        "শীঘ্রই আসছে",
+        "جلد آ رہا ہے",
+        "ਜਲਦ ਆ ਰਿਹਾ ਹੈ",
+        "kuja hivi karibuni"
+    ]
+
+    useEffect(() => {
+        const updateScreenWidth = () => {
+            setScreenWidth(window.innerWidth)
+        };
+        window.addEventListener('resize', updateScreenWidth);
+        updateScreenWidth();
+        return () => {
+            window.removeEventListener('resize', updateScreenWidth);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (screenWidth && screenWidth <= 414) {
+            setMultiple(1)
+        } else {
+            setMultiple(1.9)
+        }
+    }, [screenWidth])
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            const randomIndex = Math.floor(Math.random() * welcomeArray.length);
+            setCurrentWelcome(welcomeArray[randomIndex]);
+        }, 4000);
+
+        return () => clearTimeout(timeout);
+    }, [welcomeArray]);
 
     return (
         <div id="devbutter" className={styles.container}>
@@ -16,14 +69,15 @@ export const ComingSoonComponent = (): React.JSX.Element => {
                             DevButter!
                         </h1>
                         <p className={styles.text}>
-                            Coming Soon
+                            {currentWelcome}
                         </p>
                     </div>
                 </div>
                 <div id="image-container" className={styles.secondColumn}>
                     <div className={styles.secondColumn}>
                         <div className={styles.imageContainer}>
-                            <Image src="/manholdingcomputer.svg" alt="" width={imageDim[0] * multiple} height={imageDim[1] * multiple} layout="responsive"/>
+                            <Image src="/manholdingcomputer.svg" alt="" width={imageDim[0] * multiple} height={imageDim[1] * multiple} layout="responsive" />
+                            {/* <Image src="/person.svg" alt="" width={imageDim[0] * multiple} height={imageDim[1] * multiple} layout="responsive" /> */}
                         </div>
                     </div>
                 </div>
