7b0b7632245a14ffcf42147ff84a91ab8c6807b0
Marcos Pereira <marcos.p.10@hotmail.com>
2024-03-29 12:56:47 -0300

    feature: create a new compnent for the project container

--- src/containers/home/Projects/index.tsx
+++ src/containers/home/Projects/index.tsx
@@ -1,5 +1,23 @@
 import Image from 'next/image';
 import styles from './styles.module.scss';
+import { ProjectContainerInterface } from './interfaces';
+
+const projectContainer = ({projectData} : { projectData: ProjectContainerInterface }):React.JSX.Element => {
+    return <div key={projectData.id} className={styles.projectContainer}>
+    <div className={styles.upperRow}>
+        <Image src={projectData.imgSrc} width={10} height={10} alt={projectData.name} />
+    </div>
+    <div className={styles.underRow}>
+        <div className={styles.titleContainer}>
+            <h3>{projectData.name??"??"}</h3>
+        </div>
+        <div className={styles.skillImageContainer}>
+            <Image src="/project.svg" width={100} height={100} alt={"project image"} />
+            <Image src="/project.svg" width={100} height={100} alt={"project image"} />
+        </div>
+    </div>
+</div>
+}
 
 export const Projects = (): React.JSX.Element => {
     return (
@@ -12,35 +30,14 @@ export const Projects = (): React.JSX.Element => {
             <div id="grind-container" className={styles.secondRow}>
                 <div id="grid" className={`${styles.grid} `}>
                     {
-                        [1, 2, 3].map((el) => <div key={el} className={styles.projectContainer}>
-                            <div className={styles.upperRow}>
-                                <Image src="/project.svg" width={10} height={10} alt={"project image"} />
-                            </div>
-                            <div className={styles.underRow}>
-                                <div className={styles.titleContainer}>
-                                    <h3>project 1</h3>
-                                </div>
-                                <div className={styles.skillImageContainer}>
-                                    <Image src="/project.svg" width={100} height={100} alt={"project image"} />
-                                    <Image src="/project.svg" width={100} height={100} alt={"project image"} />
-                                </div>
-                            </div>
-                        </div>)
+                        [1, 2, 3, 4, 5, 6, 7, 8].map((el) => projectContainer({
+                            projectData: {id: el,
+                                imgSrc: `/project.svg`,
+                                name: `project ${el}`,
+                                stacks: ['React', 'Next.js', 'Typescript'],
+                                url: `/project${el}`}
+                        }))
                     }
-                    <div className={styles.projectContainer}>
-                        <div className={styles.upperRow}>
-                            <Image src="/project.svg" width={10} height={10} alt={"project image"} />
-                        </div>
-                        <div className={styles.underRow}>
-                            <div className={styles.titleContainer}>
-                                <h3>project 1</h3>
-                            </div>
-                            <div className={styles.skillImageContainer}>
-                                <Image src="/project.svg" width={100} height={100} alt={"project image"} />
-                                <Image src="/project.svg" width={100} height={100} alt={"project image"} />
-                            </div>
-                        </div>
-                    </div>
                 </div>
                 <div id="void" className={styles.void}></div>
             </div>
